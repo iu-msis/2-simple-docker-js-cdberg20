0eb63ff271c89c09f0823f3044eaf603ee929bdf
cdberg20 <63938101+cdberg20@users.noreply.github.com>
2020-09-18 17:58:27 -0400

    Fix js for profile info and v-bind for email

--- app/public/js/user.app.js
+++ app/public/js/user.app.js
@@ -1,30 +1,31 @@
 var app = new Vue({
   el: '#userProfile',
   data: {
-    userName: {},
-    userCountry: {},
-    userAge: {},
-    userEmail:{},
+    userImgLarge: "",
+    userImgSmall: "",
+    userName: "",
+    userCountry: "",
+    userAge: "",
+    userEmail: "",
   },
 
-  created() {
-    console.log('this vue app has been created!');
-  },
   created() {
     this.fetchUser();
   },
 
-      methods: {
-        fetchUser: function() {
-          fetch("https://randomuser.me/api")
-          .then( function(response) {response.json()} )
-          .then( data => {
-            var userData = data.results[0];
-            console.log(userData);
-            this.userName = userData.name.first + " " + userData.name.last;
-            this.userCountry = userData.country;
-            this.userAge = userData.age;
-            this.userEmail = userData.email;
+  methods: {
+    fetchUser: function() {
+      fetch("https://randomuser.me/api/")
+      .then(response =>response.json())
+      .then(data => {
+        var userData = data.results[0];
+        console.log(userData);
+        this.userImgLarge = userData.picture.large;
+        this.userImgSmall = userData.picture.thumbnail;
+        this.userName = userData.name.first + " " + userData.name.last;
+        this.userCountry = userData.location.country;
+        this.userAge = userData.dob.age;
+        this.userEmail = userData.email;
         });
       }
     }
