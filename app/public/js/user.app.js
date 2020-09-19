9daa3869ae4cc7ee80751ff9e651f9c8dd386f43
cdberg20 <63938101+cdberg20@users.noreply.github.com>
2020-09-18 20:19:02 -0400

    fix error with photo and email

--- app/public/js/user.app.js
+++ app/public/js/user.app.js
@@ -2,9 +2,10 @@ var app = new Vue({
   el: '#userProfile',
   data: {
     userImgLarge: "",
-    userImgSmall: "",
+    userImgThumb: "",
     userName: "",
     userCountry: "",
+    userBirthDate: "",
     userAge: "",
     userEmail: "",
   },
@@ -21,11 +22,28 @@ var app = new Vue({
         var userData = data.results[0];
         console.log(userData);
         this.userImgLarge = userData.picture.large;
-        this.userImgSmall = userData.picture.thumbnail;
+        this.userImgThumb = userData.picture.thumbnail;
         this.userName = userData.name.first + " " + userData.name.last;
         this.userCountry = userData.location.country;
+        this.userBirthDate = userData.dob.date;
         this.userAge = userData.dob.age;
         this.userEmail = userData.email;
+        })
+      .catch((error)=>{
+        console.error('Error: ',error);
+        fetch("dummy\randomuser-sample.json")
+        .then(response =>response.json())
+        .then(data => {
+          var userData = data.results[0];
+          console.log(userData);
+          this.userImgLarge = userData.picture.large;
+          this.userImgThumb = userData.picture.thumbnail;
+          this.userName = userData.name.first + " " + userData.name.last;
+          this.userCountry = userData.location.country;
+          this.userBirthDate = userData.dob.date;
+          this.userAge = userData.dob.age;
+          this.userEmail = userData.email;
+          })
         });
       }
     }
